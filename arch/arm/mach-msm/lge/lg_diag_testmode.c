ebab3ff5fb0535881605d0c352174e1e1c942e44
BobZhome <BobZhome@cheerful.com>
2013-03-02 14:52:31 -0500

    correct include path in a few LG files

--- arch/arm/mach-msm/lge/lg_diag_testmode.c
+++ arch/arm/mach-msm/lge/lg_diag_testmode.c
@@ -1,10 +1,10 @@
 #include <linux/module.h>
-#include <lg_diagcmd.h>
+#include <mach/lg_diagcmd.h>
 #include <linux/input.h>
 #include <linux/syscalls.h>
 
-#include <lg_fw_diag_communication.h>
-#include <lg_diag_testmode.h>
+#include <mach/lg_fw_diag_communication.h>
+#include <mach/lg_diag_testmode.h>
 #include <mach/qdsp5v2/audio_def.h>
 #include <linux/delay.h>
 
@@ -2145,8 +2145,8 @@ PACK (void *)LGE_Dload_SRD (PACK (void *)req_pkt_ptr, uint16 pkg_len)
 				
 			case USERDATA_BACKUP_REQUEST:
 				printk(KERN_WARNING "USERDATA_BACKUP_REQUEST");
-				//CSFB SRD remote Á¦°Å -> diag_userDataBackUp_data ¿¡¼­ Ã³¸®ÇÔ
-				//remote_rpc_srd_cmmand(req_ptr, rsp_ptr);  //userDataBackUpStart() ¿©±â¼­ ... shared ram ÀúÀå ÇÏµµ·Ï. .. 
+				//CSFB SRD remote \C1\A6\B0\C5 -> diag_userDataBackUp_data \BF\A1\BC\AD Ã³\B8\AE\C7\D4
+				//remote_rpc_srd_cmmand(req_ptr, rsp_ptr);  //userDataBackUpStart() \BF\A9\B1â¼­ ... shared ram \C0\FA\C0\E5 \C7Ïµ\B5\B7\CF. .. 
 				diag_userDataBackUp_entrySet(req_ptr,rsp_ptr,0);  //write info data ,  after rpc respons include write_sector_counter  
 
 				//CSFB SRD
@@ -2212,9 +2212,9 @@ PACK (void *)LGE_Dload_SRD (PACK (void *)req_pkt_ptr, uint16 pkg_len)
 				 }	
 				load_srd_shard_base+=1200*256 ; //mdm ram offset 
 				
-				// CSFB remote Á¦°Å -> diag_userDataBackUp_data ¿¡¼­ Ã³¸®ÇÔ
-				//remote_rpc_srd_cmmand(req_ptr, rsp_ptr);  //userDataBackUpStart() ¿©±â¼­ ... ram ÀúÀå ÇÏµµ·Ï. .. 
-				diag_userDataBackUp_entrySet(req_ptr,rsp_ptr,1);  //write info data ,  after rpc respons include write_sector_counter  remote_rpc_srd_cmmand(req_ptr, rsp_ptr);  //userDataBackUpStart() ¿©±â¼­ ... ram ÀúÀå ÇÏµµ·Ï. .. 
+				// CSFB remote \C1\A6\B0\C5 -> diag_userDataBackUp_data \BF\A1\BC\AD Ã³\B8\AE\C7\D4
+				//remote_rpc_srd_cmmand(req_ptr, rsp_ptr);  //userDataBackUpStart() \BF\A9\B1â¼­ ... ram \C0\FA\C0\E5 \C7Ïµ\B5\B7\CF. .. 
+				diag_userDataBackUp_entrySet(req_ptr,rsp_ptr,1);  //write info data ,  after rpc respons include write_sector_counter  remote_rpc_srd_cmmand(req_ptr, rsp_ptr);  //userDataBackUpStart() \BF\A9\B1â¼­ ... ram \C0\FA\C0\E5 \C7Ïµ\B5\B7\CF. .. 
 				write_size= rsp_ptr->rsp_data.write_sector_counter *256;	 //return nv backup counters  
 
 				 if( write_size >0x15000)  //384K = mode ram (300K) + mdm (80K)
